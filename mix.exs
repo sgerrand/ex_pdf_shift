4c2a9c84be2281568f2eaebcf3877c8dc68a8480
Sasha Gerrand <sgerrand@users.noreply.github.com>
2025-04-08 09:51:47 +0100

    Install and configure `ex_doc` for generating documentation

--- mix.exs
+++ mix.exs
@@ -1,34 +1,67 @@
 defmodule PDFShift.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/sgerrand/ex_pdf_shift"
+  @version "0.0.0-dev"
+
   def project do
     [
       app: :pdf_shift,
-      version: "0.0.0-dev",
-      elixir: "~> 1.17",
+      version: @version,
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+
+      # Hex
+      package: package(),
+      description: "Elixir client for the PDFShift API",
+
+      # Docs
+      name: "PDFShift",
+      source_url: @source_url,
+      homepage_url: @source_url,
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:req, "~> 0.5.0"},
       {:jason, "~> 1.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:mox, "~> 1.0", only: :test},
       {:bypass, "~> 2.1", only: :test}
     ]
   end
 
+  defp docs do
+    [
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: ["README.md"]
+    ]
+  end
+
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      maintainers: ["Sasha Gerrand"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url,
+        "Sponsor" => "https://github.com/sponsors/sgerrand"
+      },
+      files: ~w(lib mix.exs README.md)
+    ]
+  end
 end
